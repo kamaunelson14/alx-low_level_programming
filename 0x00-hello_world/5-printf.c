858974784159c14f69e5919641c06f8e9a6c1e87
Nelson_K <kamaunelson14@gmail.com>
2023-02-27 08:13:39 +0000

    Printing a string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * send formatted output to stdout
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
