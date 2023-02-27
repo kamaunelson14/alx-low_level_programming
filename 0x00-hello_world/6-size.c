7f67a3d39872dc5552fff6641ecc8da0516ec260
Nelson_K <kamaunelson14@gmail.com>
2023-02-27 09:20:48 +0000

    Print the size of various types on stdout

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,15 @@
  */
 int main(void)
 {
-	char a; int b; long c; long long d; float e;
-        printf("Size of a char: %2ld byte(s)\n",sizeof(a));
-	printf("Size of an int: %2ld byte(s)\n",sizeof(b));
-	printf("Size of a long int: %2ld byte(s)\n",sizeof(c));
-	printf("Size of a long long int: %2ld byte(s)\n",sizeof(d));
-	printf("Size of a float: %2ld byte(s)\n",sizeof(e));
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
+ 	printf("Size of a char: %2ld byte(s)\n", sizeof(a));
+	printf("Size of an int: %2ld byte(s)\n", sizeof(b));
+	printf("Size of a long int: %2ld byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %2ld byte(s)\n", sizeof(d));
+	printf("Size of a float: %2ld byte(s)\n", sizeof(e));
 	return (0);
 }
