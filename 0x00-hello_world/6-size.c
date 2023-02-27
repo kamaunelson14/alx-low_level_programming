23fd09295a0eccdec2818cf98288725bcc43f38a
Nelson_K <kamaunelson14@gmail.com>
2023-02-27 09:12:06 +0000

    Printing the size of various tpes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a; int b; long c; long long d; float e;
+        printf("Size of a char: %2ld byte(s)\n",sizeof(a));
+	printf("Size of an int: %2ld byte(s)\n",sizeof(b));
+	printf("Size of a long int: %2ld byte(s)\n",sizeof(c));
+	printf("Size of a long long int: %2ld byte(s)\n",sizeof(d));
+	printf("Size of a float: %2ld byte(s)",sizeof(e));
+	return (0);
+}
